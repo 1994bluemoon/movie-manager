f1efc334438ead847768c0733b0b28c6b06f76b9
Henry Duong <minhhoang94_bluemoon@yahoo.com.vn>
2018-03-20 01:47:55 +0700

    henry

---
+++ app/src/main/java/com/example/dminh/moviesmanager/Adapters/MovieRecyclerAdapter.java
@@ -0,0 +1,88 @@
+package com.example.dminh.moviesmanager.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dminh.moviesmanager.Activities.MovieDetailActivity;
+import com.example.dminh.moviesmanager.Models.Movie;
+import com.example.dminh.moviesmanager.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by dminh on 11/24/2017.
+ */
+
+public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieRecyclerAdapter.ViewHolder> {
+
+    List<Movie> movies;
+    Context context;
+
+    public MovieRecyclerAdapter(Context context, List<Movie> movies){
+        this.movies = movies;
+        this.context = context;
+    }
+
+    private Context getContext(){
+        return context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext())
+                .load(movie.getPosterPath())
+                .into(holder.ivMovieImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        @BindView(R.id.ivMovieImage)
+        ImageView ivMovieImage;
+        @BindView(R.id.tvTitle)
+        TextView tvTitle;
+        @BindView(R.id.tvOverview)
+        TextView tvOverview;
+        @BindView(R.id.cvMovie)
+        CardView cvMovie;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Movie movie = movies.get(getAdapterPosition());
+            Intent intent = new Intent(getContext(), MovieDetailActivity.class);
+            intent.putExtra("MOVIE", movie);
+            getContext().startActivity(intent);
+        }
+    }
+}
